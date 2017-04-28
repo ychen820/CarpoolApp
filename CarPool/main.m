2734f6397bb5fe903e614d7264a278f581e758c1
Yang Chen <ychen207@binghamton.edu>
2017-04-27 19:41:03 -0500

    Initial Commit

---
+++ CarPool/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CarPool
+//
+//  Created by Nathan Chen on 4/27/17.
+//  Copyright © 2017 Nathan Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
